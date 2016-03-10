cfdc7ddfc0f8cb2e0fd3040b069c1f8fb7c247d2
Christopher Hutchinson <cshutchinson@gmail.com>
2016-03-10 15:47:00 -0700

    clean up files that aren't specific to application

--- public/src/index.js
+++ public/src/index.js
@@ -15,4 +15,5 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
-  , document.getElementById('body'));
+  , document.getElementById('body')
+);

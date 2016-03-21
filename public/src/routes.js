897b4ef4ec1416ddf50d8764cb69f10c50217815
Christopher Hutchinson <cshutchinson@gmail.com>
2016-03-21 13:26:02 -0600

    routes.js changes for home

--- public/src/routes.js
+++ public/src/routes.js
@@ -7,9 +7,19 @@ import ChildNew from './pages/ChildNew';
 import TaskList from './pages/ChildTasks';
 import TransactionList from './pages/ChildTransactions';
 
+// export default (
+//   <Route path="/main" component={App}>
+//     <IndexRoute component={ChildIndex} />
+//     <Route path="child/new" component={ChildNew} />
+//     <Route path="children/tasks/:id" component={TaskList} />
+//     <Route path="children/transactions/:id" component={TransactionList} />
+//   </Route>
+// );
+
 export default (
-  <Route path="/main" component={App}>
-    <IndexRoute component={ChildIndex} />
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="main" component={ChildIndex} />
     <Route path="child/new" component={ChildNew} />
     <Route path="children/tasks/:id" component={TaskList} />
     <Route path="children/transactions/:id" component={TransactionList} />
